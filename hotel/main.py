6a86e5ca3a6f21a0e69eb0752dcaf17839a602c0
aliasofeaa <101005810+aliasofeaa@users.noreply.github.com>
2024-02-23 17:19:25 +0800

    Update main.py

--- hotel/main.py
+++ hotel/main.py
@@ -9,7 +9,7 @@ print("Welcome to FairviewHotel")
 # Define rates
 room_rates = {'Single': 100, 'Double': 150, 'Suite': 250}
 
-# Ask user to select their room type
+# Prompt user to select room type
 while True:
     room_type = input("Enter room type (Single/Double/Suite): ")
     if room_type in room_rates:
@@ -17,40 +17,40 @@ while True:
     else:
         print("Invalid room type. Please choose based on the option only !")
 
-# Ask user to enter number of rooms
+# Prompt user to enter number of rooms
 while True:
-    num_rooms = input("Enter number of rooms: ")
-    if num_rooms.isdigit() and int(num_rooms) > 0:
+    number_rooms = input("Enter number of rooms: ")
+    if number_rooms.isdigit() and int(number_rooms) > 0:
         break
     else:
         print("Invalid number of rooms. Please enter a valid number !")
 
-# Ask user to enter how many nights they want to stay
+# Prompt user to enter number of nights
 while True:
-    num_nights = input("Enter number of nights: ")
-    if num_nights.isdigit() and int(num_nights) > 0:
+    number_nights = input("Enter number of nights: ")
+    if number_nights.isdigit() and int(number_nights) > 0:
         break
     else:
         print("Invalid number of nights. Please enter a valid number !")
 
-num_rooms = int(num_rooms)
-num_nights = int(num_nights)
+number_rooms = int(number_rooms)
+number_nights = int(number_nights)
 rate = room_rates[room_type]
 
 # Calculate total cost
-total_cost = rate * num_rooms * num_nights
+total_cost = rate * number_rooms * number_nights
 
 # Apply discounts and promotions
-if num_rooms > 5:
+if number_rooms > 5:
     total_cost *= 0.9  # 10% discount
     print("You got 10% of discount !")
 else:
     print("Sorry you didn't get any discount. ")
-if room_type == 'Single' and num_nights > 7:
+if room_type == 'Single' and number_nights > 7:
     print("Congratulations! You've got a complimentary breakfast voucher.")
 else:
     print("Sorry you didn't get any complimentary breakfast voucher ")
-if room_type == 'Suite' and num_nights < 3:
+if room_type == 'Suite' and number_nights < 3:
     print("Minimum stay for a Suite is 3 nights.")
     total_cost = None
 
